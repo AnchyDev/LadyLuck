c8d3ab2a277a6b593a3a4d8ef5abdcdb3fc17827
AnchyDev <lewienato@gmail.com>
2024-06-17 16:11:10 +1000

    Fixed creature template for new model template changes in core and made them re-runnable.

--- data/sql/db-world/updates/ladyluck_world_2022_12_20_02.sql
+++ data/sql/db-world/updates/ladyluck_world_2022_12_20_02.sql
@@ -1,1 +1,2 @@
+DELETE FROM `ladyluck_lottery_loot` WHERE item_id = 18640;
 INSERT INTO `ladyluck_lottery_loot` (`item_id`, `item_count`, `level_min`, `level_max`, `roll`, `comment`) VALUES (18640, 5, 0, 80, 100, 'Happy Fun Rock');
